3c4f177e0bf4ba6938cc266b62b43fc0b73ce652
AyoAlonge16 <theayoalonge@gmail.com>
2023-10-10 14:46:59 -0700

    edit for char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1 and errno set accordingly.
+ */
 int _putchar(char c);
 
 #endif /* MAIN_H */
@@ -9,17 +15,22 @@ int _putchar(char c);
 /* 0-main.c */
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
