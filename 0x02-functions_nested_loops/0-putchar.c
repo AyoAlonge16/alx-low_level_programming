dbbb22a863e8dfa5aa193d8ee06b563a80d0b491
AyoAlonge16 <theayoalonge@gmail.com>
2023-10-10 14:05:38 -0700

    change made to putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
+/* 0-main.c */
+#include "main.h"
 
 int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
 }
