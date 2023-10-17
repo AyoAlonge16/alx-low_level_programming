eba45835e244f82c68ea3cf53837c1ce1340e8bc
AyoAlonge16 <theayoalonge@gmail.com>
2023-10-16 21:41:54 -0700

    Done with mandatory tasks

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+
+ * _strlen - this will return the length of a string s
+
+ * s: string
+
+ * Return: length
+
+ */
+
+int _strlen(char *s)
+
+{
+
+	int longi = 0;
+
+
+	while (*s != '\0')
+
+	{
+
+		longi++;
+
+		s++;
+
+	}
+
+
+	return (longi);
+
+}
